203d979560514bbee90b23a6627150804db3d28a
Jeevesh Gangwar <jeevesh.gangwar_cs18@gla.ac.in>
2020-02-19 22:12:30 +0545

    2

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         while (a != 5) {
             System.out.println("Press 1 to add a new contact\n" +
                     "Press 2 to view all contacts\n" +
-                    "Press 3 to search for a contact\n" +
+                    "Press 3 to search for a contacts\n" +
                     "Press 4 to delete a contact\n" +
                     "Press 5 to exit program ");
             int ch = sc.nextInt();
